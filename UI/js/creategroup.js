a77783f49f57e1f9c14f509f7c71cf791db7018f
nanfuka <kalungi2k6@gmail.com>
2019-04-03 12:11:26 +0300

    Add java script to the create group feature [Finishes #165010252]

---
+++ UI/js/creategroup.js
@@ -0,0 +1,41 @@
+document.getElementById('creategroup').addEventListener('click', Compose);
+
+const creategroup = 'http://127.0.0.1:5000/api/v2/groups';
+
+function Compose(e){
+    e.preventDefault();
+    token = localStorage.getItem('token');
+    if (token === null) {
+        alert('please login to access this route');
+
+    };
+
+    let name = document.getElementById('name').value;
+    let role = document.getElementById('role').value;
+    
+    let data = {
+        name: name,
+        role: role
+    }
+
+
+    fetch(creategroup, {
+        method: 'POST',
+        // mode: 'cors',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(response => {
+        // console.log(response);
+        if (response.status === 201){
+            document.getElementById("positiveresponse").innerHTML = "group created"
+        } else {
+            document.getElementById("negativeresponse").innerHTML = response.error
+        }
+    })
+    .catch(err => console.log(err));
+}
